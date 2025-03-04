02391d0a6de7bcf9c52fa795a4c46bd45caeb714
Hamid Ali <hamidali4218867@gmail.com>
2025-03-04 17:54:37 +0500

    refactor: modify server setup for Vercel deployment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
